b0b5268995af84fc21d49b73f75662cd9005d518
pcolumbro <pcolumbro@users.noreply.github.com>
2023-02-25 01:49:55 -0800

    auth0

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 
+const domain = process.env.AUTH_CLIENT_DOMAIN || '';
+const clientId = process.env.AUTH_CLIENT_ID || '';
+
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Auth0Provider domain={domain} clientId={clientId}>
+				<Route path="/" element={<App />} />
+			</Auth0Provider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
---
+++ src/pages/report-builder/report-builder.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+import RecordBuilder from "../../features/add-record/record-builder";
+
+export const ReportBuilder = () => {
+	return (
+		<Container>
+			<RecordBuilder />
+		</Container>
+	);
+};
+export default ReportBuilder;
