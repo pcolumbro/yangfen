d9f8120a45f8d4db1ac468e71781aed9cb8885a5
pcolumbro <pcolumbro@users.noreply.github.com>
2023-02-25 01:23:04 -0800

    basics for report builder page

--- src/features/add-record/record-builder.tsx
+++ src/features/add-record/record-builder.tsx
@@ -1,24 +1,43 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, Grid, Stack, TextField } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 const RecordBuilder = () => {
 
 	const [query, setQuery] = useState<string>('');
 
-	return(
-		<Box>
-			<FormControl>
-				<InputLabel></InputLabel>
-				<TextField
-					variant="outlined"
-					size="small"
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-				/>
-			</FormControl>
-		</Box>			
+	return (
+		<>
+			<Box>
+				<Grid container>
+					<Grid item xs={12} md={3}>
+						<Stack spacing={2}>
+							<FormControl fullWidth>
+								<TextField
+									id="search"
+									label="Supplement Search"
+									variant="outlined"
+									size="small"
+									value={query}
+									onChange={e => setQuery(e.target.value)}
+								/>
+							</FormControl>
+							<Button color='info' variant='contained'>Add Custom Suppplement</Button>
+							<Stack spacing={2}>
+
+							</Stack>
+						</Stack>
+					</Grid>
+					<Grid item xs={12} md={9}>
+						<Box>
+							Report Display
+						</Box>
+					</Grid>
+				</Grid>
+			</Box>
+			<Box></Box>
+		</>
 	);
 };
 export default RecordBuilder;
